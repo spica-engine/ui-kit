39fda8e7b69500b7494a1f150e70f5a35860396f
idriska <idriscumali@gmail.com>
2025-01-14 15:28:14 +0300

    feat: create flex element component (#5)

---
+++ src/components/atoms/flex-element/FlexElement.tsx
@@ -0,0 +1,56 @@
+import { FC, memo, ReactElement } from "react";
+import styles from "./FlexElement.module.scss";
+
+export type FlexElementProps = {
+  children?: ReactElement;
+  alignment?:
+    | "leftTop"
+    | "top"
+    | "rightTop"
+    | "leftCenter"
+    | "center"
+    | "rightCenter"
+    | "leftBottom"
+    | "bottom"
+    | "rightBottom";
+  direction?: "vertical" | "horizontal" | "wrap";
+  dimensionX?: number | "fill" | "hug";
+  dimensionY?: number | "fill" | "hug";
+  gap?: number;
+  className?: string;
+};
+
+const FlexElement: FC<FlexElementProps> = memo(
+  ({
+    children,
+    alignment = "center",
+    direction = "horizontal",
+    dimensionX = "hug",
+    dimensionY = "hug",
+    gap = 5,
+    className = "",
+  }) => {
+    const alignmentClass = styles[alignment];
+    const directionClass = styles[direction === "vertical" ? "vertical" : "horizontal"];
+    const wrapClass = styles[direction === "wrap" ? "wrap" : "noWrap"];
+    const dimensionClassX = typeof dimensionX === "number" ? "" : styles[`dimensionX${dimensionX}`];
+    const dimensionClassY = typeof dimensionY === "number" ? "" : styles[`dimensionY${dimensionY}`];
+
+    const inlineStyles = {
+      gap: `${gap}px`,
+      width: typeof dimensionX === "number" ? `${dimensionX}px` : undefined,
+      height: typeof dimensionY === "number" ? `${dimensionY}px` : undefined,
+    };
+
+    return (
+      <div
+        style={inlineStyles}
+        className={`${styles.flexElement} ${alignmentClass} ${directionClass} ${wrapClass} ${dimensionClassX} ${dimensionClassY} ${className}`}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+export default FlexElement;
