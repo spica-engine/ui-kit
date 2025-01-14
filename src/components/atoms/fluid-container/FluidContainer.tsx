ebaa28c184637a50a571a7fdd021528532cede15
idriska <idriscumali@gmail.com>
2025-01-14 15:32:23 +0300

    feat: create fluid container component (#6)

---
+++ src/components/atoms/fluid-container/FluidContainer.tsx
@@ -0,0 +1,23 @@
+import { FC, memo } from "react";
+import FlexElement, { FlexElementProps } from "../flex-element/FlexElement";
+
+type FluidContainerProps = {
+  prefix?: FlexElementProps;
+  root?: FlexElementProps;
+  suffix?: FlexElementProps;
+  className?: string;
+} & FlexElementProps;
+
+const FluidContainer: FC<FluidContainerProps> = memo(({ prefix, root, suffix, ...props }) => {
+  return (
+    <FlexElement {...props}>
+      <>
+        {prefix?.children && <FlexElement {...prefix} />}
+        {root?.children && <FlexElement {...root} />}
+        {suffix?.children && <FlexElement {...suffix} />}
+      </>
+    </FlexElement>
+  );
+});
+
+export default FluidContainer;
