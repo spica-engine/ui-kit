6701143ebd201c4311f82d0518ad2e42089ad82c
kenangsmv <rio.kenan@gmail.com>
2025-01-15 13:58:14 +0300

    feat: create location input

---
+++ src/components/atoms/inputs/normal/location/Location.tsx
@@ -0,0 +1,59 @@
+import React, { FC, memo, useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import styles from "./Location.module.scss";
+import "leaflet/dist/leaflet.css";
+import FlexElement from "components/atoms/flex-element/FlexElement";
+import FluidContainer from "components/atoms/fluid-container/FluidContainer";
+import Icon from "components/atoms/icon/Icon";
+import Text from "components/atoms/text/Text";
+
+type LocationProps = {
+  coordinates: [number, number];
+  title: string;
+};
+
+const Location: FC<LocationProps> = ({ coordinates, title }) => {
+
+  return (
+    <FlexElement
+      className={styles.location}
+      dimensionX={500}
+      dimensionY={500}
+      gap={10}
+      direction="vertical"
+      alignment="leftCenter"
+    >
+      <>
+        <FluidContainer
+          prefix={{ children: <Icon name="mapMarker" className={styles.icon}/> }}
+          root={
+            {
+              children: <Text variant="secondary">{title}</Text>
+            }
+          }
+          dimensionX="fill"
+          alignment="leftCenter"
+          dimensionY={36}
+        />
+
+        <MapContainer
+          center={coordinates || [51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          className={styles.map}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {coordinates && (
+            <Marker position={coordinates}>
+            </Marker>
+          )}
+        </MapContainer>
+      </>
+    </FlexElement>
+  );
+};
+
+export default Location;
