70d08440a4193b17f2e9b38a061d258cac4e081e
kenangsmv <rio.kenan@gmail.com>
2025-01-15 17:20:32 +0300

    feat: add date picker component

---
+++ src/components/atoms/inputs/normal/date/DatePicker.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import type { DatePickerProps } from "antd";
+import { DatePicker } from "antd";
+import dayjs from "dayjs";
+import Icon from "components/atoms/icon/Icon";
+import styles from "./DatePicker.module.scss";
+import FluidContainer from "components/atoms/fluid-container/FluidContainer";
+
+type DateRangePickerProps = {
+  onChange: DatePickerProps["onChange"];
+  value?: dayjs.Dayjs;
+  placeholder?: string;
+};
+
+const DatePickerInput: React.FC<DateRangePickerProps> = ({
+  onChange,
+  value,
+  placeholder,
+}) => (
+  <FluidContainer
+    className={styles.datePickerContainer}
+    alignment={"leftCenter"}
+    dimensionX={"fill"}
+    prefix={{ children: <Icon name="calendarBlank" /> }}
+    root={{
+      children: (
+        <DatePicker
+          onChange={onChange}
+          value={value ? dayjs(value) : null}
+          className={styles.datePicker}
+          suffixIcon={null}
+          placeholder={placeholder}
+        />
+      ),
+    }}
+  />
+);
+
+export default DatePickerInput;
