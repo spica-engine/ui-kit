0f71b37067877d7b0fddf3f6170ccb5921ad4198
Kanan Gasimov <57861260+kenangsmv@users.noreply.github.com>
2025-01-15 11:42:00 +0300

    refactor: implement better type notation for flex element (#13)

--- src/components/atoms/text/Text.tsx
+++ src/components/atoms/text/Text.tsx
@@ -1,21 +1,21 @@
-import React, { FC, memo } from 'react'
+import React, { FC, memo, ReactElement } from 'react'
 import FlexElement, { FlexElementProps } from '../flex-element/FlexElement'
 import styles from './Text.module.scss'
 
 type TypeText = {
     variant?: 'primary' | 'secondary' | 'danger';
     size?: 'small' | 'medium' | 'large' | 'xlarge';
-    text: string;
-};
+    children: ReactElement | string;
+} & FlexElementProps;
 
-const Text: FC<TypeText & FlexElementProps> = memo(
-    ({ variant = 'primary', size = 'medium', text, ...props }) => {
+const Text: FC<TypeText> = memo(
+    ({ variant = 'primary', size = 'medium', children, ...props }) => {
         const variantClass = styles[variant];
         const sizeClass = styles[size];
 
         return (
             <FlexElement {...props}>
-                <span className={`${styles.text} ${variantClass} ${sizeClass}`}>{text}</span>
+                <span className={`${styles.text} ${variantClass} ${sizeClass}`}>{children}</span>
             </FlexElement>
         );
     }
