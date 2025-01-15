89439be96de0e6be42f1b0ff8e0ad9bfc403b4dd
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-01-15 10:52:27 +0300

    change request

--- src/components/atoms/text/Text.tsx
+++ src/components/atoms/text/Text.tsx
@@ -1,25 +1,21 @@
 import React, { FC, memo } from 'react'
-import FlexElement from '../flex-element/FlexElement'
+import FlexElement, { FlexElementProps } from '../flex-element/FlexElement'
 import styles from './Text.module.scss'
 
 type TypeText = {
     variant?: 'primary' | 'secondary' | 'danger';
     size?: 'small' | 'medium' | 'large' | 'xlarge';
-    children: string;
-    dimensionX?: 'fill' | 'hug';
-    dimensionY?: 'fill' | 'hug';
+    text: string;
 };
 
-const Text: FC<TypeText> = memo(
-    ({ variant = 'primary', size = 'medium', children, dimensionX = 'hug', dimensionY = 'hug' }) => {
+const Text: FC<TypeText & FlexElementProps> = memo(
+    ({ variant = 'primary', size = 'medium', text, ...props }) => {
         const variantClass = styles[variant];
         const sizeClass = styles[size];
-        const fillXClass = dimensionX === 'fill' ? styles.fill : '';
-        const fillYClass = dimensionY === 'fill' ? styles.fill : '';
 
         return (
-            <FlexElement dimensionX={dimensionX} dimensionY={dimensionY}>
-                <span className={`${styles.text} ${variantClass} ${sizeClass} ${fillXClass} ${fillYClass}`}>{children}</span>
+            <FlexElement {...props}>
+                <span className={`${styles.text} ${variantClass} ${sizeClass}`}>{text}</span>
             </FlexElement>
         );
     }
