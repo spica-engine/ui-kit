d2949cb76603ef4d6375dcf922ac7a089093ef3a
kenangsmv <rio.kenan@gmail.com>
2025-01-16 11:21:57 +0300

    refactor: manage better input type changes

--- src/components/atoms/input-with-icon/InputWithIcon.tsx
+++ src/components/atoms/input-with-icon/InputWithIcon.tsx
@@ -1,20 +1,40 @@
 import React, { FC, ReactElement } from "react";
-import FluidContainer, { FluidContainerProps } from "../fluid-container/FluidContainer";
+import FluidContainer, {
+  FluidContainerProps,
+} from "../fluid-container/FluidContainer";
 import styles from "./InputWithIcon.module.scss";
-
 
 type TypeInputWithIcon = {
   className?: string;
+  placeholder?: string;
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
 } & FluidContainerProps;
 
 const InputWithIcon: FC<TypeInputWithIcon> = ({
   className,
+  placeholder,
+  inputProps,
   ...props
 }) => {
-  return <FluidContainer 
-    className={`${styles.container} ${className}`}   
-    {...props}
-  />;
+  return (
+    <FluidContainer
+      className={`${styles.container} ${className}`}
+      dimensionY={36}
+      root={{
+        children: (
+          <input
+            type={inputProps?.type || "text"}
+            {...inputProps}
+            placeholder={placeholder}
+            className={styles.input}
+          />
+        ),
+        dimensionX: "fill",
+        alignment: "leftCenter",
+      }}
+      {...props}
+    />
+  );
 };
 
 export default InputWithIcon;
