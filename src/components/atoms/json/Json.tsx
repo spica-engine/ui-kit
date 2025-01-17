e8d0e462e98099b1f4d45d1c5404fcebb609933e
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-01-17 18:42:45 +0300

    feat: add json display (#30)

---
+++ src/components/atoms/json/Json.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+import Text from '../text/Text'
+import styles from './Json.module.scss'
+
+type TypeJson = {
+    data: Record<string, unknown>;
+};
+
+const Json: React.FC<TypeJson> = memo(({ data }) => {
+    return <Text className={styles.jsonContainer}>{JSON.stringify(data, null, 2)}</Text>;
+});
+
+export default Json;
