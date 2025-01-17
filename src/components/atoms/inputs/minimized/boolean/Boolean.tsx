1ec7d3fa3004353ab3527a4d4f523a82f4ec94ac
idriska <idriscumali@gmail.com>
2025-01-17 18:50:44 +0300

    feat: create boolean input minimized (#24)

---
+++ src/components/atoms/inputs/minimized/boolean/Boolean.tsx
@@ -0,0 +1,19 @@
+import { FC, memo } from "react";
+import styles from "./Boolean.module.scss";
+import { FlexElementProps } from "components/atoms/flex-element/FlexElement";
+import Switch from "components/atoms/switch/Switch";
+
+export type TypeInputBooleanMinimized = {
+  checked: boolean;
+  disabled?: boolean;
+  containerProps?: FlexElementProps;
+  onChange?: (checked: boolean) => void;
+};
+
+const InputBooleanMinimized: FC<TypeInputBooleanMinimized> = memo(
+  ({ checked, disabled = false, containerProps, onChange }) => {
+    return <Switch checked={checked} disabled={disabled} {...containerProps} onChange={onChange} />;
+  }
+);
+
+export default InputBooleanMinimized;
