3e0abe11011240aea1ea94c852ef6f3624b16c96
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-01-17 16:57:53 +0300

    feat: change request app component fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import StringMinimized from 'components/atoms/inputs/minimized/string/StringMinimized';
 
 function App() {
-  const [inputValue, setInputValue] = React.useState('');
-
-  const handleValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleClear = () => {
-    setInputValue('');
-  };
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -27,15 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
-        <StringMinimized
-          value={inputValue} // Pass the value
-          onChange={handleValueChange} // Pass the change handler
-          onClear={handleClear} // Pass the clear handler
-          placeholder="Enter text here..."
-          type="text"
-          
-        />
+        </a>
       </header>
     </div>
   );
