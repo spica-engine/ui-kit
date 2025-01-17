e8648149290f399cc33053e3c8fa6b77f119dac3
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-01-17 18:49:48 +0300

    feat: add string minimized input (#29)

---
+++ src/components/atoms/inputs/minimized/string/StringMinimized.tsx
@@ -0,0 +1,39 @@
+import React, { FC, memo, useState } from 'react'
+import InputWithIcon from 'components/atoms/input-with-icon/InputWithIcon'
+import styles from './StringMinimized.module.scss'
+import IconButton from 'components/atoms/icon-button/IconButton';
+
+type TypeStringMinimized = {
+    onClear?: () => void
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+const StringMinimized: FC<TypeStringMinimized> = memo(
+    ({ onClear, ...props }) => {
+        return (
+            <InputWithIcon
+                className={styles.stringMinimized}
+                inputProps={{
+                    ...props,
+                }}
+                suffix={{
+                    children: (
+                        <IconButton
+                            icon="close"
+                            variant="base"
+                            buttonProps={{
+                                onClick: onClear,
+                            }}
+                        />
+                    ),
+                    dimensionX: 'hug',
+                    alignment: 'center',
+                }}
+                alignment="leftCenter"
+                dimensionX="fill"
+                dimensionY={36}
+            />
+        );
+    }
+);
+
+export default StringMinimized
