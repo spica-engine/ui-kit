67deef272149520c64b9a81b5ec3692e4c727865
kenangsmv <rio.kenan@gmail.com>
2025-01-20 19:07:54 +0300

    change request

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -1,11 +1,10 @@
 import FlexElement, { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
 import React, {
   memo,
-  useEffect,
   InputHTMLAttributes,
   ChangeEventHandler,
-  CSSProperties,
   KeyboardEvent,
+  forwardRef,
 } from "react";
 import styles from "./Input.module.scss";
 
@@ -14,50 +13,33 @@ type TypeInput<T = string> = TypeFlexElement & {
   type?: InputHTMLAttributes<HTMLInputElement>["type"];
   placeholder?: string;
   isFocused?: boolean;
-  className?: string;
-  style?: CSSProperties;
   onChange?: ChangeEventHandler<HTMLInputElement>;
   onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 };
 
-const Input = <T extends string | number | readonly string[]>({
-  value,
-  type,
-  placeholder,
-  className,
-  style,
-  isFocused,
-  onChange,
-  onKeyDown,
-  ...props
-}: TypeInput<T>) => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+const Input = forwardRef<HTMLInputElement, TypeInput<string | number | readonly string[]>>(
+  ({ value, type, placeholder, isFocused, onChange, onKeyDown, ...props }, ref) => {
+    const flexContainerProps: TypeFlexElement = {
+      ...props,
+      dimensionX: "fill",
+    };
 
-  const flexContainerProps: TypeFlexElement = {
-    ...props,
-    dimensionX: "fill",
-  };
+    return (
+      <FlexElement {...flexContainerProps}>
+        <input
+          ref={ref}
+          type={type || "text"}
+          value={value}
+          placeholder={placeholder || "Enter value"}
+          className={styles.input}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      </FlexElement>
+    );
+  }
+);
 
-  useEffect(() => {
-    if (isFocused) {
-      inputRef.current?.focus();
-    }
-  }, [isFocused]);
-
-  return (
-    <FlexElement {...flexContainerProps} className={className}>
-      <input
-        ref={inputRef}
-        type={type || "text"}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder || "Enter value"}
-        className={`${styles.input} ${className}`}
-        style={style}
-        onKeyDown={onKeyDown}
-      />
-    </FlexElement>
-  );
-};
-
-export default memo(Input) as <T = string>(props: TypeInput<T>) => React.ReactElement;
+export default memo(Input) as <T = string>(
+  props: TypeInput<T> & { ref?: React.Ref<HTMLInputElement> }
+) => React.ReactElement;
