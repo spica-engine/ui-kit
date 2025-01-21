9f7046e15fa7fbf8f8f16c11a1871a3d95495a43
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-01-21 18:09:01 +0300

    feat: add modal footer (#44)

---
+++ src/components/atoms/modal/footer/ModalFooter.tsx
@@ -0,0 +1,32 @@
+import FluidContainer, { TypeFluidContainer } from 'components/atoms/fluid-container/FluidContainer';
+import React, { FC, memo } from 'react'
+import styles from './ModalFooter.module.scss'
+
+type TypeModalFooter = {
+    className?: string;
+} & TypeFluidContainer;
+
+const ModalFooter: FC<TypeModalFooter> = memo((
+  { className, prefix, root, suffix, ...props }
+  ) => {
+    return (
+        <FluidContainer 
+            className= {`${styles.modalFooter} ${className}`}
+            {...{ alignment:"leftCenter", dimensionX:"fill", dimensionY: "hug", ...props }}
+            prefix={{
+                children: prefix?.children,
+                ...{ dimensionX: "hug", alignment:"leftCenter", ...prefix },
+            }}
+            root={{
+                children: root?.children,
+                ...{ dimensionX: "fill", alignment: "leftCenter", ...root },
+            }}
+            suffix={{
+                children: suffix?.children,
+                ...{ dimensionX: "hug", alignment:"rightCenter", ...suffix },
+            }}
+        />
+    )
+})
+
+export default ModalFooter
