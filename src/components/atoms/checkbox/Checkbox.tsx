29388f5e3a82f20b6aba6159696790a28f7ca3f5
idriska <idriscumali@gmail.com>
2025-01-21 10:59:05 +0300

    feat: create checkbox atom (#38)

---
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -0,0 +1,50 @@
+import React, { FC, memo, ReactNode, useState } from "react";
+import styles from "./Checkbox.module.scss";
+import FluidContainer, { TypeFluidContainer } from "../fluid-container/FluidContainer";
+import Text from "../text/Text";
+
+type TypeCheckbox = {
+  checked?: boolean;
+  disabled?: boolean;
+  label?: ReactNode;
+  indeterminate?: boolean;
+  onChange?: (checked: boolean) => void;
+};
+
+const Checkbox: FC<TypeCheckbox & TypeFluidContainer> = ({
+  checked,
+  disabled,
+  label,
+  indeterminate,
+  onChange,
+  ...props
+}) => {
+  const [value, setValue] = useState(checked);
+
+  const handleClick = () => {
+    if (disabled) return;
+    setValue(!value);
+    onChange?.(!value);
+  };
+
+  return (
+    <FluidContainer
+      {...props}
+      dimensionY={36}
+      onClick={handleClick}
+      className={`${props.className} ${styles.container} ${disabled && styles.disabled}`}
+      prefix={{
+        children: (
+          <div className={`${styles.checkbox} ${indeterminate && styles.indeterminate}`}>
+            <input type="checkbox" checked={value} />
+            <label htmlFor="checkbox" />
+          </div>
+        ),
+        ...props.prefix,
+      }}
+      root={{ children: <Text>{label}</Text>, ...props.root }}
+    />
+  );
+};
+
+export default memo(Checkbox);
