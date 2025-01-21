fe94ee877ab00d12f732ee442af16c2a3593f53d
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-01-21 11:42:35 +0300

    feat: add modal header (#41)

---
+++ src/components/atoms/modal/header/ModalHeader.tsx
@@ -0,0 +1,28 @@
+import FluidContainer, { TypeFluidContainer } from 'components/atoms/fluid-container/FluidContainer';
+import React, { FC, memo } from 'react'
+import styles from './ModalHeader.module.scss'
+
+type TypeModalHeader = {
+    className?: string;
+} & TypeFluidContainer;
+
+const ModalHeader: FC<TypeModalHeader> = memo((
+  { className, prefix, root, ...props }
+  ) => {
+    return (
+        <FluidContainer 
+            className= {`${styles.modalHeader} ${className}`}
+            {...{ alignment:"leftCenter", dimensionX:"fill", dimensionY: 36, ...props }}
+            prefix={{
+                children: prefix?.children,
+                ...{ dimensionX: "hug", alignment:"leftCenter", ...prefix },
+            }}
+            root={{
+                children: root?.children,
+                ...{ dimensionX: "fill", alignment: "leftCenter", ...root },
+            }}
+        />
+    )
+})
+
+export default ModalHeader
