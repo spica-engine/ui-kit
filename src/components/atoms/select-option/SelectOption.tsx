111182f4e34d5248c299aee878ef63739a029e68
idriska <idriscumali@gmail.com>
2025-01-24 16:21:35 +0300

    feat: create select component (#42)

---
+++ src/components/atoms/select-option/SelectOption.tsx
@@ -0,0 +1,76 @@
+import { FC, memo, useLayoutEffect, useRef, useState } from "react";
+import styles from "./SelectOption.module.scss";
+import FluidContainer, { TypeFluidContainer } from "../fluid-container/FluidContainer";
+import Checkbox from "../checkbox/Checkbox";
+
+export type TypeLabeledValue = {
+  value: string | number;
+  label: string;
+};
+
+type TypeSelectOption = {
+  option: string | number | TypeLabeledValue;
+  selected?: boolean;
+  multiple?: boolean;
+  disabled?: boolean;
+  onSelect: (value: string | number) => void;
+};
+
+const SelectOption: FC<TypeSelectOption & TypeFluidContainer> = ({
+  option,
+  selected,
+  multiple,
+  disabled,
+  onSelect,
+  ...props
+}) => {
+  const [displayerWidth, setDisplayerWidth] = useState(0);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const { value, label } =
+    typeof option === "object"
+      ? { value: option.value, label: option.label }
+      : { value: option, label: option };
+
+  const handleClick = (value: string | number) => {
+    if (disabled) return;
+    onSelect(value);
+  };
+
+  useLayoutEffect(() => {
+    if (!containerRef.current) return;
+    setDisplayerWidth(containerRef.current?.offsetWidth - 50);
+  }, []);
+
+  return (
+    <FluidContainer
+      ref={containerRef}
+      key={value}
+      dimensionX="fill"
+      dimensionY={36}
+      alignment="leftCenter"
+      onClick={() => handleClick(value)}
+      className={`${styles.option} ${selected && styles.selected} ${disabled && styles.disabled}`}
+      root={{
+        children: (
+          <Checkbox
+            disabled={disabled}
+            dimensionX="fill"
+            checked={selected}
+            label={label}
+            alignment="leftCenter"
+            prefix={multiple ? undefined : { children: null }}
+            root={{
+              className: `${props.root?.className} ${styles.displayer}`,
+            }}
+            labelProps={{ style: { maxWidth: displayerWidth }, className: styles.displayer }}
+          />
+        ),
+        dimensionX: "fill",
+      }}
+      {...props}
+    />
+  );
+};
+
+export default memo(SelectOption);
