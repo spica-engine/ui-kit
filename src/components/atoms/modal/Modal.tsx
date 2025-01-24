18da4d6e2fa11e329ab090f32c5942eb6bd01542
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-01-24 17:20:07 +0300

    feat: add backdrop

--- src/components/atoms/modal/Modal.tsx
+++ src/components/atoms/modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { TypeFluidContainer } from 'components/atoms/fluid-container/FluidContainer';
-import React, { FC, memo, useState } from 'react'
+import React, { FC, memo, useRef, useState } from 'react'
 import styles from './Modal.module.scss'
 import FlexElement from '../flex-element/FlexElement';
 import ModalHeader from './header/ModalHeader';
@@ -12,6 +12,8 @@ type TypeModal = {
     className?: string;
     animation?: 'leftToMiddle' | 'rightToMiddle' | 'topToBottom' | 'bottomToTop' | 'growFromCenter' | 'zoomIn';
     showCloseButton?: boolean;
+    showBackdrop?: boolean;
+    closeOnBackdropClick?: boolean;
     children: React.ReactNode;
     onClose?: () => void;
 } & TypeFluidContainer;
@@ -20,16 +22,25 @@ const ModalComponent: FC<TypeModal> = ({
     className,
     animation = 'growFromCenter',
     showCloseButton = true,
+    showBackdrop = true,
+    closeOnBackdropClick = false,
     children,
     onClose,
     ...props
 }) => {
 
     const [isVisible, setIsVisible] = useState(true);
+    const modalRef = useRef<HTMLDivElement>(null);
 
     const handleClose = () => {
         setIsVisible(false);
         if (onClose) onClose();
+    };
+
+    const handleOutsideClick = (event: React.MouseEvent) => {
+        if (closeOnBackdropClick && modalRef.current && !modalRef.current.contains(event.target as Node)) {
+            handleClose();
+        }
     };
 
     if (!isVisible) return null;
@@ -37,11 +48,12 @@ const ModalComponent: FC<TypeModal> = ({
 
     return (
         <>
-            <div className={styles.modalBackdrop} />
+            {showBackdrop && <div className={styles.modalBackdrop} onClick={handleOutsideClick} />}
             <FlexElement
                 className={`${styles.modalContainer} ${styles[animation]}`}
                 alignment='top'
                 direction='vertical'
+                ref={modalRef}
                 {...props}
             >
                 {showCloseButton && (
