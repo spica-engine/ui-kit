75c05b5ab87f27485e67e8e79e48c8b0b5ae2c11
idriska <idriscumali@gmail.com>
2025-01-27 14:12:35 +0300

    feat: add mode to the fluid container (#54)

--- src/components/atoms/fluid-container/FluidContainer.tsx
+++ src/components/atoms/fluid-container/FluidContainer.tsx
@@ -1,21 +1,32 @@
 import { memo, forwardRef } from "react";
-import FlexElement, { TypeFlexElement } from "../flex-element/FlexElement";
+import FlexElement, { TypeDimension, TypeFlexElement } from "../flex-element/FlexElement";
 
 export type TypeFluidContainer = {
   prefix?: TypeFlexElement;
   root?: TypeFlexElement;
   suffix?: TypeFlexElement;
+  mode?: "hug" | "fill" | "middle";
 } & TypeFlexElement;
 
 const FluidContainer = forwardRef<HTMLDivElement, TypeFluidContainer>(
-  ({ prefix, root, suffix, ...props }, ref) => {
+  ({ prefix, root, suffix, mode = "hug", alignment, ...props }, ref) => {
+    const getDimensionX = (element: TypeFlexElement | undefined, defaultMode: TypeDimension) =>
+      element?.dimensionX || defaultMode;
+
+    const prefixDimensionX = getDimensionX(prefix, "hug");
+    const rootDimensionX = getDimensionX(root, mode === "fill" ? "fill" : "hug");
+    const suffixDimensionX = getDimensionX(suffix, "hug");
+
+    const containerAlignment = alignment || (mode === "middle" ? "center" : "leftCenter");
+
+    const renderFlexElement = (element: TypeFlexElement | undefined, dimensionX: TypeDimension) =>
+      element?.children ? <FlexElement {...element} dimensionX={dimensionX} /> : null;
+
     return (
-      <FlexElement {...props} ref={ref}>
-        <>
-          {prefix?.children && <FlexElement {...prefix} />}
-          {root?.children && <FlexElement {...root} />}
-          {suffix?.children && <FlexElement {...suffix} />}
-        </>
+      <FlexElement {...props} ref={ref} alignment={containerAlignment}>
+        {renderFlexElement(prefix, prefixDimensionX)}
+        {renderFlexElement(root, rootDimensionX)}
+        {renderFlexElement(suffix, suffixDimensionX)}
       </FlexElement>
     );
   }
