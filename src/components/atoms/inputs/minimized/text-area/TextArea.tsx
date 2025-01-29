58f1a34036a21078526322f138c5863d2804f375
kenangsmv <rio.kenan@gmail.com>
2025-01-29 16:34:09 +0300

    feat: create minimized text area component

---
+++ src/components/atoms/inputs/minimized/text-area/TextArea.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import FluidContainer, {
+  TypeFluidContainer,
+} from "components/atoms/fluid-container/FluidContainer";
+import Icon from "components/atoms/icon/Icon";
+import styles from "./TextArea.module.scss";
+
+type MinimizedTextAreaProps = {
+  value?: string;
+  onClear?: () => void;
+  onChange?: (value: string) => void;
+  onFocus?: (isFocused: boolean) => void;
+  onBlur?: (isFocused: boolean) => void;
+  focusedRows?: number;
+  rows?: number;
+  cols?: number;
+} & TypeFluidContainer;
+
+const MinimizedTextArea = ({
+  value,
+  onClear,
+  onChange,
+  focusedRows = 3,
+  onFocus,
+  onBlur,
+  rows = 1,
+  cols = 1,
+  ...props
+}: MinimizedTextAreaProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    onFocus?.(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    onBlur?.(false);
+  };
+
+  return (
+    <FluidContainer
+      mode="fill"
+      dimensionX="fill"
+      root={{
+        children: (
+          <textarea
+            value={value}
+            onChange={(e) => onChange?.(e.target.value)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            rows={isFocused ? focusedRows : rows}
+            cols={cols}
+          ></textarea>
+        ),
+      }}
+      suffix={{ children: <Icon name="close" /> }}
+      {...props}
+      className={`${styles.textArea} ${props.className}`}
+    />
+  );
+};
+
+export default MinimizedTextArea;
