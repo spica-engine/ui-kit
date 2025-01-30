ece709b26e404db992eb8de44046d3c73eec238a
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-01-30 15:11:59 +0300

    feat: add multiple selection input component

---
+++ src/components/atoms/inputs/normal/multiple-selection/MultipleSelection.tsx
@@ -0,0 +1,71 @@
+import BaseInput from "components/atoms/base-input/BaseInput";
+import Icon from "components/atoms/icon/Icon";
+import Text from "components/atoms/text/Text";
+import React, { memo, useRef, useState } from "react";
+import styles from "./MultipleSelection.module.scss";
+import Select from "components/molecules/select/Select";
+import { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
+
+type TypeMultipleSelection<T = string | number> = {
+  label: string;
+  value?: T[];
+  options?: T[];
+  onChange?: (value: T[]) => void;
+} & TypeFlexElement;
+
+const MultipleSelection = <T extends string | number>({
+  label,
+  value = [],
+  options,
+  onChange,
+  ...props
+}: TypeMultipleSelection<T>) => {
+  const selectRef = useRef<any>(null);
+
+  const [forceFocus, setForceFocus] = useState(false);
+
+  const handleOnFocusChange = (isFocused: boolean) => {
+    selectRef?.current?.toggleDropdown(isFocused);
+    setForceFocus(isFocused);
+  };
+
+  return (
+    <BaseInput
+      dimensionX={"fill"}
+      forceFocus={forceFocus}
+      onFocusChange={(isFocused) => handleOnFocusChange(isFocused)}
+      labelProps={{
+        dimensionX: "hug",
+        divider: true,
+        prefix: {
+          children: <Icon className={styles.icon} name="formatListChecks" />,
+        },
+        root: {
+          dimensionX: "hug",
+          children: (
+            <Text className={styles.text} size="medium">
+              {label}
+            </Text>
+          ),
+        },
+      }}
+      inputContainerProps={{ className: styles.baseInput }}
+      {...props}
+    >
+      <Select
+        className={styles.select}
+        selectRef={selectRef}
+        disableClick
+        options={options || []}
+        value={value}
+        placeholder=""
+        multiple={true}
+        onChange={(value) => {
+          onChange?.(value as T[]);
+        }}
+      />
+    </BaseInput>
+  );
+};
+
+export default memo(MultipleSelection);
