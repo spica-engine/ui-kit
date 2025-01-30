5b087d23d98bf4f5a41681c020707877f9eba18a
Kanan Gasimov <57861260+kenangsmv@users.noreply.github.com>
2025-01-30 16:34:42 +0300

    feat: wrap date picker with base input (#79)

--- src/components/atoms/inputs/normal/date/Date.tsx
+++ src/components/atoms/inputs/normal/date/Date.tsx
@@ -1,35 +1,46 @@
-import React, { memo } from "react";
-import type { DatePickerProps } from "antd";
-import { DatePicker } from "antd";
+import React, { memo, useState } from "react";
 import dayjs from "dayjs";
 import Icon from "components/atoms/icon/Icon";
 import styles from "./Date.module.scss";
 import FluidContainer from "components/atoms/fluid-container/FluidContainer";
-
+import BaseInput from "components/atoms/base-input/BaseInput";
+import Text from "components/atoms/text/Text";
+import DatePicker from "components/atoms/date-picker/DatePicker";
+import type { DatePickerProps } from "antd";
+import { Dayjs } from "dayjs";
 type DateRangePickerProps = {
-  onChange: DatePickerProps["onChange"];
-  value?: dayjs.Dayjs;
+  label?: string;
+  onChange?: (date: Dayjs, dateString: string | string[]) => void;
+  value?: Dayjs | null;
   placeholder?: string;
 };
 
-const DatePickerInput: React.FC<DateRangePickerProps> = ({ onChange, value, placeholder }) => (
-  <FluidContainer
-    className={styles.datePickerContainer}
-    alignment={"leftCenter"}
-    dimensionX={"fill"}
-    prefix={{ children: <Icon name="calendarBlank" /> }}
-    root={{
-      children: (
-        <DatePicker
-          onChange={onChange}
-          value={value ? dayjs(value) : null}
-          className={styles.datePicker}
-          suffixIcon={null}
-          placeholder={placeholder}
-        />
-      ),
-    }}
-  />
-);
+const DateInput: React.FC<DateRangePickerProps> = ({ label, onChange, value, placeholder }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [pickerFocused, setPickerFocused] = useState(false);
 
-export default memo(DatePickerInput);
+  const onFocusChange = (isFocused: boolean) => {
+    setIsFocused(isFocused);
+    setPickerFocused(isFocused);
+  };
+
+  return (
+    <BaseInput
+      dimensionX="fill"
+      alignment="leftCenter"
+      labelProps={{
+        prefix: { children: <Icon name="calendarBlank" /> },
+        root: { children: <Text>{label}</Text> },
+      }}
+      inputContainerProps={{
+        dimensionX: "fill",
+        alignment: "leftCenter",
+      }}
+      onFocusChange={onFocusChange}
+    >
+      <DatePicker suffixIcon={null} onChange={onChange} value={value} />
+    </BaseInput>
+  );
+};
+
+export default memo(DateInput);
