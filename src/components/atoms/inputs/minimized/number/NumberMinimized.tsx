1ebf9f570d588fa7dad8a06298cfa8a7c44e41ae
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-02-06 11:53:53 +0300

    feat: add number input minimized (#87)

---
+++ src/components/atoms/inputs/minimized/number/NumberMinimized.tsx
@@ -0,0 +1,91 @@
+import Button from "components/atoms/button/Button";
+import FluidContainer, {
+  TypeFluidContainer,
+} from "components/atoms/fluid-container/FluidContainer";
+import Icon from "components/atoms/icon/Icon";
+import Input from "components/atoms/input/Input";
+import Select, { TypeSelectRef } from "components/molecules/select/Select";
+import React, { FC, memo, useRef } from "react";
+import styles from "./NumberMinimized.module.scss";
+
+type TypeStringMinimized = {
+  onClear?: () => void;
+  value?: number;
+  options?: number[];
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+  onChange?: (value: number) => void;
+} & TypeFluidContainer;
+
+const NumberMinimized: FC<TypeStringMinimized> = ({
+  onClear,
+  value,
+  options,
+  inputProps,
+  onChange,
+  ...props
+}) => {
+  const selectRef = useRef<TypeSelectRef>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClear = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    onClear?.();
+
+    if (selectRef.current) {
+      selectRef.current.clear();
+    }
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  return (
+    <FluidContainer
+      alignment="leftCenter"
+      dimensionX="fill"
+      className={styles.numberMinimized}
+      {...props}
+      root={{
+        dimensionX: "fill",
+        alignment: "leftCenter",
+        children: options ? (
+          <Select
+            selectRef={selectRef}
+            className={styles.select}
+            placeholder={value !== undefined ? String(value) : ""}
+            value={value}
+            options={options || []}
+            onChange={(value) => onChange?.(value as number)}
+          />
+        ) : (
+          <Input
+            ref={inputRef}
+            value={value ?? ""}
+            type="number"
+            {...inputProps}
+            onChange={(event) => onChange?.(Number(event.target.value))}
+          />
+        ),
+        ...props.root,
+      }}
+      suffix={{
+        dimensionX: "hug",
+        alignment: "center",
+        children: (
+          <Button
+            children={<Icon name="close" />}
+            color="transparent"
+            onClick={handleClear}
+            className={styles.closeIcon}
+          />
+        ),
+        ...props.suffix,
+      }}
+    />
+  );
+};
+
+export default memo(NumberMinimized);
