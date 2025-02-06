7cf93fe0f9ffe009023e0baaf08b70a94ad08c15
idriska <idriscumali@gmail.com>
2025-02-06 18:52:53 +0300

    refactor: date picker props (#103)

--- src/components/atoms/date-picker/DatePicker.tsx
+++ src/components/atoms/date-picker/DatePicker.tsx
@@ -4,10 +4,24 @@ import type { DatePickerProps as AntDatePickerProps } from "antd";
 import dayjs, { Dayjs } from "dayjs";
 import styles from "./DatePicker.module.scss";
 
-type DatePickerProps = AntDatePickerProps;
+type DatePickerProps = {
+  onChange?: (value: string | string[]) => void;
+  value: Date | string | null | undefined;
+} & Omit<AntDatePickerProps, "value" | "onChange">;
 
-const DatePicker = ({ ...props }: DatePickerProps) => {
-  return <AntDatePicker {...props} className={`${styles.datePicker} ${props.className}`} />;
+const DatePicker = ({ value, onChange, ...props }: DatePickerProps) => {
+  const handleOnChange = (date: Dayjs, dateString: string | string[]) => {
+    onChange?.(dateString);
+  };
+
+  return (
+    <AntDatePicker
+      {...props}
+      value={value ? dayjs(value) : null}
+      onChange={handleOnChange}
+      className={`${styles.datePicker} ${props.className}`}
+    />
+  );
 };
 
 export default DatePicker;
