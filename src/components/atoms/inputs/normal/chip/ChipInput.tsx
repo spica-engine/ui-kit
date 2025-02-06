88a281e96e7906ebda9db738e91100887d9cc840
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-02-06 18:48:53 +0300

    feat: add chip input component (#98)

---
+++ src/components/atoms/inputs/normal/chip/ChipInput.tsx
@@ -0,0 +1,55 @@
+import Input from "components/atoms/input/Input";
+import React, { FC, memo, useState } from "react";
+import Chip from "components/atoms/chip/Chip";
+import styles from "./ChipInput.module.scss";
+import FlexElement, { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
+
+type TypeChipInput = {
+  label?: string[];
+  placeholder?: string;
+} & TypeFlexElement;
+
+const ChipInput: FC<TypeChipInput> = ({
+  label,
+  placeholder = "Enter a value than press enter",
+  ...props
+}) => {
+  const [chips, setChips] = useState<string[]>(label || []);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      e.preventDefault();
+      const newChips = [...chips, inputValue.trim()];
+      setChips(newChips);
+      setInputValue("");
+    }
+  };
+
+  const handleDelete = (index: number) => {
+    const updatedChips = chips.filter((_, i) => i !== index);
+    setChips(updatedChips);
+  };
+
+  return (
+    <FlexElement
+      dimensionX={"fill"}
+      direction="wrap"
+      alignment="leftCenter"
+      {...props}
+      className={`${styles.chipInputContainer} ${props.className}`}
+    >
+      {chips.map((chip, index) => (
+        <Chip key={index} label={chip} onDelete={() => handleDelete(index)} />
+      ))}
+      <Input
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </FlexElement>
+  );
+};
+
+export default memo(ChipInput);
