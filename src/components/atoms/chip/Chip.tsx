ba55f3e8657ea24a0fd628db8a15ba2ea82033db
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-02-06 13:44:50 +0300

    feat: add chip component (#93)

---
+++ src/components/atoms/chip/Chip.tsx
@@ -0,0 +1,62 @@
+import React, { FC, memo } from "react";
+import FluidContainer, { TypeFluidContainer } from "../fluid-container/FluidContainer";
+import styles from "./Chip.module.scss";
+import Icon from "../icon/Icon";
+import Button from "../button/Button";
+import { IconName } from "utils/iconList";
+
+type TypeChip = {
+  label?: string;
+  variant?: "outlined" | "filled";
+  icon?: IconName;
+  onDelete?: () => void;
+  suffixIcon?: IconName;
+  suffixOnClick?: () => void;
+} & TypeFluidContainer;
+
+const Chip: FC<TypeChip> = ({
+  label,
+  variant = "filled",
+  icon,
+  onDelete,
+  suffixIcon,
+  suffixOnClick,
+  ...props
+}) => {
+  return (
+    <FluidContainer
+      dimensionX="hug"
+      gap={10}
+      prefix={{
+        dimensionX: "hug",
+        children: icon ? <Icon name={icon} /> : undefined,
+        ...props.prefix,
+      }}
+      root={{
+        children: label,
+        ...props.root,
+      }}
+      suffix={
+        (suffixIcon || onDelete) && {
+          dimensionX: "hug",
+          alignment: "center",
+          children: (
+            <Button
+              className={styles.closeButton}
+              shape="circle"
+              color="transparent"
+              onClick={suffixIcon ? suffixOnClick : onDelete}
+            >
+              <Icon name={suffixIcon || "close"} />
+            </Button>
+          ),
+          ...props.suffix,
+        }
+      }
+      {...props}
+      className={`${props.className} ${styles.chip} ${styles[variant]} ${!onDelete && !suffixIcon ? styles.noButton : ""}`}
+    />
+  );
+};
+
+export default memo(Chip);
