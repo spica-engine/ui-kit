efefd46961f844a314fd3f7034e454111980db3f
kenangsmv <rio.kenan@gmail.com>
2025-02-07 10:36:49 +0300

    change request

--- src/components/atoms/list-row/ListRow.tsx
+++ src/components/atoms/list-row/ListRow.tsx
@@ -1,26 +1,19 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, memo, ReactNode } from "react";
 import FlexElement, { TypeFlexElement } from "../flex-element/FlexElement";
 import styles from "./ListRow.module.scss";
 
-type ListRowProps = {
-  children: ReactNode;
+type TypeListRow = {
+  ref?: React.Ref<HTMLDivElement>;
 } & TypeFlexElement;
 
-type ListCellProps = {
-  children: ReactNode;
-} & TypeFlexElement;
-
-const ListCell: FC<ListCellProps> = ({ children, ...props }) => {
-  return (
-    <FlexElement dimensionX="hug" {...props} className={`${props.className}`}>
-      {children}
-    </FlexElement>
-  );
+type TypeListRowComponent = React.FC<TypeListRow> & {
+  Cell: typeof FlexElement;
 };
 
-const ListRow: FC<ListRowProps> & { Cell: FC<ListCellProps> } = ({ children, ...props }) => {
+const ListRowComponent: FC<TypeListRow> = ({ children, ...props }) => {
   return (
     <FlexElement
+      ref={props.ref}
       dimensionX="fill"
       alignment="leftCenter"
       gap={10}
@@ -31,6 +24,16 @@ const ListRow: FC<ListRowProps> & { Cell: FC<ListCellProps> } = ({ children, ...
     </FlexElement>
   );
 };
+
+const ListCell = memo(({ children, ...props }: TypeFlexElement) => {
+  return (
+    <FlexElement dimensionX="hug" {...props} className={`${props.className}`}>
+      {children}
+    </FlexElement>
+  );
+});
+
+const ListRow = memo(ListRowComponent) as unknown as TypeListRowComponent;
 
 ListRow.Cell = ListCell;
 
