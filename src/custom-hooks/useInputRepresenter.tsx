7fad685fca7289ba1686c21f1f9178ae1aa10ca3
idriska <idriscumali@gmail.com>
2025-02-10 10:16:41 +0300

    refactor: input representer types

--- src/custom-hooks/useInputRepresenter.tsx
+++ src/custom-hooks/useInputRepresenter.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { TypeCoordinates } from "components/atoms/map/Map";
 import { Fragment, ReactNode } from "react";
 import StringInput from "../components/atoms/inputs/normal/string/String";
@@ -11,19 +12,18 @@ import MultipleSelectionInput from "../components/atoms/inputs/normal/multiple-s
 import LocationInput from "../components/atoms/inputs/normal/location/Location";
 import RichTextInput from "../components/atoms/inputs/normal/rich-text/RichText";
 import Icon from "components/atoms/icon/Icon";
-import { isTypeSpicaLocation } from "utils/helper";
 import ObjectInput from "components/atoms/inputs/normal/object/ObjectInput";
 import ArrayInput from "components/atoms/inputs/normal/array/ArrayInput";
+import { apiUtils } from "utils/apiUtils";
 
 export type TypeProperties = {
   [key: string]: {
     type: keyof typeof types;
-    key: string;
     title: string;
     description: string;
     options?: TypeOptions;
     enum?: (string | number)[];
-    default?: boolean;
+    default?: TypeValueType;
     items?: TypeMultipleItems | TypeArrayItems;
     minItems?: number;
     maxItems?: number;
@@ -31,6 +31,12 @@ export type TypeProperties = {
     className?: string;
     properties?: TypeProperties;
   };
+};
+
+export type TypeValueType = string | number | boolean | string[] | number[] | object;
+
+export type TypeRepresenterValue = {
+  [key: string]: TypeValueType;
 };
 
 export type TypeInputType =
@@ -48,10 +54,9 @@ export type TypeInputType =
   | "array";
 
 type TypeOptions = {
-  position: "top" | "bottom" | "left" | "right";
+  position?: "top" | "bottom" | "left" | "right";
+  index?: boolean;
 };
-
-export type TypeSpicaLocation = { type: string; coordinates: [number, number] };
 
 type TypeMultipleItems = {
   title?: string;
@@ -59,7 +64,7 @@ type TypeMultipleItems = {
   enum?: (number | string)[];
 };
 
-type TypeArrayItems = {
+export type TypeArrayItems = {
   title?: string;
   type: TypeInputType;
   properties: TypeProperties;
@@ -76,27 +81,40 @@ export type TypeInputProps<T> = {
   description: string;
   value?: T;
   className?: string;
-  properties?: TypeProperties;
-  options?: any[];
-  minItems?: number;
-  maxItems?: number;
-  items?: any;
   onChange?: ({ key, value }: TypeChangeEvent<T>) => void;
 };
 
+type TypeObjectInputProps<T> = {
+  properties?: TypeProperties;
+} & TypeInputProps<T>;
+
+type TypeSelectInputProps<T extends string | number> = {
+  enum?: T[];
+} & TypeInputProps<T>;
+
+type TypeMultiSelectInputProps<T extends string | number> = {
+  enum?: T[];
+} & TypeInputProps<T[]>;
+
+type TypeArrayInputProps<T> = {
+  minItems?: number;
+  maxItems?: number;
+  items?: TypeArrayItems;
+} & TypeInputProps<T>;
+
 export type TypeInputTypeMap = {
-  string: (props: TypeInputProps<string>) => ReactNode;
-  number: (props: TypeInputProps<number>) => ReactNode;
+  string: (props: TypeSelectInputProps<string>) => ReactNode;
+  number: (props: TypeSelectInputProps<number>) => ReactNode;
   textarea: (props: TypeInputProps<string>) => ReactNode;
   date: (props: TypeInputProps<Date | string | null>) => ReactNode;
   boolean: (props: TypeInputProps<boolean>) => ReactNode;
   color: (props: TypeInputProps<string>) => ReactNode;
   storage: (props: TypeInputProps<string>) => ReactNode;
-  multiselect: (props: TypeInputProps<(string | number)[]>) => ReactNode;
-  location: (props: TypeInputProps<TypeCoordinates | TypeSpicaLocation>) => ReactNode;
+  multiselect: (props: TypeMultiSelectInputProps<string | number>) => ReactNode;
+  location: (props: TypeInputProps<TypeCoordinates | apiUtils.TypeLocation>) => ReactNode;
   richtext: (props: TypeInputProps<string>) => ReactNode;
-  object: (props: TypeInputProps<any>) => ReactNode;
-  array: (props: TypeInputProps<any>) => ReactNode;
+  object: (props: TypeObjectInputProps<TypeRepresenterValue>) => ReactNode;
+  array: (props: TypeArrayInputProps<TypeValueType>) => ReactNode;
 };
 
 const types: TypeInputTypeMap = {
@@ -106,7 +124,7 @@ const types: TypeInputTypeMap = {
       description={props.description}
       inputContainerClassName={props.className}
       value={props.value}
-      options={props.options}
+      options={props.enum}
       onChange={(value) => props.onChange?.({ key: props.key, value })}
     />
   ),
@@ -116,6 +134,7 @@ const types: TypeInputTypeMap = {
       description={props.description}
       inputContainerClassName={props.className}
       value={props.value}
+      options={props.enum}
       onChange={(value) => props.onChange?.({ key: props.key, value })}
     />
   ),
@@ -141,6 +160,7 @@ const types: TypeInputTypeMap = {
     <BooleanInput
       checked={props.value}
       label={props.title}
+      description={props.description}
       containerProps={{ dimensionX: "fill" }}
       onChange={(value) => props.onChange?.({ key: props.key, value })}
     />
@@ -169,18 +189,19 @@ const types: TypeInputTypeMap = {
       description={props.description}
       inputContainerClassName={props.className}
       value={props.value}
+      options={props.enum}
       onChange={(value) => props.onChange?.({ key: props.key, value })}
     />
   ),
   location: (props) => {
-    if (isTypeSpicaLocation(props.value)) {
+    if (apiUtils.isTypeLocation(props.value)) {
       const coordinates = props?.value.coordinates;
       props.value = { lat: coordinates[1], lng: coordinates[0] };
     }
 
     const handleChangeLocation = (value: TypeCoordinates) => {
-      let normalizedValue: TypeSpicaLocation | TypeCoordinates = value;
-      if (isTypeSpicaLocation(props.value)) {
+      let normalizedValue: apiUtils.TypeLocation | TypeCoordinates = value;
+      if (apiUtils.isTypeLocation(props.value)) {
         normalizedValue = {
           type: "Point",
           coordinates: [value.lng, value.lng],
@@ -233,9 +254,7 @@ const types: TypeInputTypeMap = {
 
 type TypeUseInputRepresenter = {
   properties: TypeProperties;
-  value: {
-    [key: string]: any;
-  };
+  value: TypeRepresenterValue;
   onChange?: (event: TypeChangeEvent<unknown>) => void;
 };
 
@@ -250,13 +269,13 @@ const useInputRepresenter = ({ properties, value, onChange }: TypeUseInputRepres
     return (
       <Fragment key={key}>
         {types[el.type]({
-          key: el.key,
+          key,
           title: el.title,
           description: el.description,
           value: el.type === "object" ? value?.[key] : value,
           className: el.className,
           properties: el.properties,
-          options: el.enum,
+          enum: el.enum as any,
           minItems: el.minItems,
           maxItems: el.maxItems,
           items: el.items,
---
+++ src/utils/apiUtils.ts
@@ -0,0 +1,12 @@
+export namespace apiUtils {
+    export type TypeLocation = { type: string; coordinates: [number, number] };
+
+    export const isTypeLocation = (obj: any): obj is TypeLocation => {
+        return typeof obj === "object" &&
+            obj !== null &&
+            typeof obj.type === "string" &&
+            Array.isArray(obj.coordinates) &&
+            obj.coordinates.length === 2 &&
+            obj.coordinates.every((coord: unknown) => typeof coord === "number")
+    }
+}
