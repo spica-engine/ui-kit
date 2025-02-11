b221a54ede4245e32019ef2532a065ea9424eef2
idriska <idriscumali@gmail.com>
2025-02-11 18:26:18 +0300

    feat: create theming system (#114)

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "theme/createTheme";
+import { ThemeProvider, useTheme } from "theme/ThemeContext";
+import { loadTheme } from "./loadTheme";
+import { saveTheme } from "./saveTheme";
+
+export {
+    ThemeProvider,
+    useTheme,
+    createTheme,
+    loadTheme,
+    saveTheme
+};
