1002b9335ffdb15196ac6d6a457ea52e53531fbd
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-02-11 15:56:01 +0300

    change request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
 import React from "react";
-import logo from "./logo.svg";
 import styles from "./App.module.scss";
+import MenuGroup from "components/organisms/menu-group/MenuGroup";
+import Button from "components/atoms/button/Button";
+import Icon from "components/atoms/icon/Icon";
 
 function App() {
-  return <div className={styles.app}>Hello</div>;
+  const menuOptions = [
+    [
+      { prefix: <Icon name="formatSize" />, value: "Configure the viewwwwwwwww" },
+      { prefix: <Icon name="security" />, value: "Configure rules" },
+    ],
+    [
+      {
+        prefix: (
+          <Button
+            children={undefined}
+            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
+          />
+        ),
+        value: "History",
+      },
+      {
+        prefix: (
+          <Button
+            children={undefined}
+            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
+          />
+        ),
+        value: "Limitations",
+      },
+      {
+        prefix: (
+          <Button
+            children={undefined}
+            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
+          />
+        ),
+        value: "Read Only",
+      },
+    ],
+  ];
+
+  return (
+    <div className={styles.app}>
+      <MenuGroup options={menuOptions} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/organisms/menu-group/MenuGroup.tsx
+++ src/components/organisms/menu-group/MenuGroup.tsx
@@ -1,22 +1,48 @@
-import FlexElement, { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
 import React, { FC, memo } from "react";
+import FlexElement, { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
+import FluidContainer from "components/atoms/fluid-container/FluidContainer";
+import Text from "components/atoms/text/Text";
 import styles from "./MenuGroup.module.scss";
 
 type TypeMenuGroup = {
-  children: React.ReactNode;
+  options?: { prefix?: React.ReactNode; value: string }[][];
 } & TypeFlexElement;
 
-const MenuGroup: FC<TypeMenuGroup> = ({ children, ...props }) => {
+const MenuGroup: FC<TypeMenuGroup> = ({ options = [], ...props }) => {
   return (
     <FlexElement
       dimensionX={160}
-      gap={0}
       direction="vertical"
-      alignment="center"
+      gap={0}
+      alignment="leftCenter"
+      className={styles.container}
       {...props}
-      className={`${styles.container} ${props.className}`}
     >
-      {children}
+      {options.map((section, sectionIndex) => (
+        <FlexElement
+          key={sectionIndex}
+          dimensionX={"fill"}
+          direction="vertical"
+          gap={0}
+          className={styles.menuSection}
+        >
+          {section.map((option, optionIndex) => (
+            <FluidContainer
+              gap={10}
+              key={optionIndex}
+              dimensionX={"fill"}
+              prefix={{
+                children: option.prefix,
+                dimensionX: 14,
+                dimensionY: 14,
+                className: styles.prefix,
+              }}
+              root={{ children: <Text className={styles.root}>{option.value}</Text> }}
+              className={styles.menuItem}
+            />
+          ))}
+        </FlexElement>
+      ))}
     </FlexElement>
   );
 };
