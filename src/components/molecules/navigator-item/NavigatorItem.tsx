31e1dda3fe1e5162a56294aeb63b1f3e35af520d
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-02-12 16:46:29 +0300

    feat: add navigator item (#112)

---
+++ src/components/molecules/navigator-item/NavigatorItem.tsx
@@ -0,0 +1,68 @@
+import FluidContainer, {
+  TypeFluidContainer,
+} from "components/atoms/fluid-container/FluidContainer";
+import Icon from "components/atoms/icon/Icon";
+import React, { FC, memo } from "react";
+import styles from "./NavigatorItem.module.scss";
+import { IconName } from "utils/iconList";
+import Text from "components/atoms/text/Text";
+import Button from "components/atoms/button/Button";
+import FlexElement from "components/atoms/flex-element/FlexElement";
+
+type SuffixIcon = {
+  name: IconName;
+  onClick?: () => void;
+};
+
+type TypeNavigatorItem = {
+  label: string;
+  prefixIcon?: IconName;
+  suffixIcons?: SuffixIcon[];
+} & TypeFluidContainer;
+
+const NavigatorItem: FC<TypeNavigatorItem> = ({
+  label,
+  prefixIcon,
+  suffixIcons = [],
+  ...props
+}) => {
+  return (
+    <FluidContainer
+      dimensionX={"fill"}
+      dimensionY={36}
+      mode="fill"
+      prefix={
+        prefixIcon && {
+          children: <Icon name={prefixIcon} />,
+        }
+      }
+      root={{
+        children: (
+          <Text alignment="leftCenter" dimensionX={"fill"}>
+            {label}
+          </Text>
+        ),
+      }}
+      suffix={{
+        children: suffixIcons.length > 0 && (
+          <>
+            {suffixIcons.map(({ name, onClick }, index) => (
+              <Button
+                key={index}
+                color="transparent"
+                className={styles.suffixButton}
+                onClick={onClick}
+              >
+                <Icon name={name} />
+              </Button>
+            ))}
+          </>
+        ),
+      }}
+      {...props}
+      className={`${styles.navigatorItem} ${props.className}`}
+    />
+  );
+};
+
+export default memo(NavigatorItem);
