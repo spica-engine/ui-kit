74908ab76cac31c8bf2f0dda917c3ee6e4dacc34
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-02-13 12:00:31 +0300

    change request

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,9 @@
 import React from "react";
+import logo from "./logo.svg";
 import styles from "./App.module.scss";
-import MenuGroup from "components/organisms/menu-group/MenuGroup";
-import Button from "components/atoms/button/Button";
-import Icon from "components/atoms/icon/Icon";
 
 function App() {
-  const menuOptions = [
-    [
-      { prefix: <Icon name="formatSize" />, value: "Configure the viewwwwwwwww" },
-      { prefix: <Icon name="security" />, value: "Configure rules" },
-    ],
-    [
-      {
-        prefix: (
-          <Button
-            children={undefined}
-            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
-          />
-        ),
-        value: "History",
-      },
-      {
-        prefix: (
-          <Button
-            children={undefined}
-            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
-          />
-        ),
-        value: "Limitations",
-      },
-      {
-        prefix: (
-          <Button
-            children={undefined}
-            style={{ height: "14px", width: "14px", padding: 0, minWidth: 0 }}
-          />
-        ),
-        value: "Read Only",
-      },
-    ],
-  ];
-
-  return (
-    <div className={styles.app}>
-      <MenuGroup options={menuOptions} />
-    </div>
-  );
+  return <div className={styles.app}>Hello</div>;
 }
 
 export default App;
