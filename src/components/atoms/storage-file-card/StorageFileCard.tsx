936ac87ceb8e8a09def3d8914f2ab34c70d2db2e
idriska <idriscumali@gmail.com>
2025-02-14 18:06:39 +0300

    refactor: create useFileView hook and change file type (#125)

--- src/components/atoms/storage-file-card/StorageFileCard.tsx
+++ src/components/atoms/storage-file-card/StorageFileCard.tsx
@@ -1,51 +1,24 @@
 import { FC, memo } from "react";
 import styles from "./StorageFileCard.module.scss";
 import FlexElement, { TypeFlexElement } from "../flex-element/FlexElement";
+import useFileView from "custom-hooks/useFileView";
+import { TypeFile } from "utils/interface";
 import Text, { TypeText } from "../text/Text";
 import Icon from "../icon/Icon";
 
 export type TypeStorageFileCard = {
-  name: string;
-  contentType: string;
-  url: string;
+  file: TypeFile;
   viewProps?: TypeFlexElement;
   nameProps?: TypeText;
 };
 
-const contentTypeMapping = [
-  { regex: /^image\//, viewer: (url: string, name?: string) => <img src={url} alt={name} /> },
-  { regex: /^video\//, viewer: () => <Icon name="movie" className={styles.icon} /> },
-  {
-    regex: /^(text\/plain|text\/javascript|application\/json)$/,
-    viewer: (url: string, contentType: string) => <embed type={contentType} src={url} />,
-  },
-  {
-    regex: /^(application\/vnd\.openxmlformats-officedocument\.spreadsheetml\.sheet|text\/csv)$/,
-    viewer: () => <Icon name="gridOn" className={styles.icon} />,
-  },
-  {
-    regex: /^application\/pdf/,
-    viewer: (url: string, contentType: string) => <embed type={contentType} src={url} />,
-  },
-  { regex: /^application\/zip/, viewer: () => <Icon name="folderZip" className={styles.icon} /> },
-];
-
 const StorageFileCard: FC<TypeStorageFileCard & TypeFlexElement> = ({
-  name,
-  url,
-  contentType,
+  file,
   viewProps,
   nameProps,
   ...props
 }) => {
-  const renderContentView = () => {
-    const match = contentTypeMapping.find(({ regex }) => regex.test(contentType));
-    if (match) {
-      return match.viewer(url || "", contentType);
-    }
-
-    return <Icon name="fileDocument" className={styles.icon} />;
-  };
+  const fileView = useFileView({ file });
 
   return (
     <FlexElement
@@ -62,10 +35,10 @@ const StorageFileCard: FC<TypeStorageFileCard & TypeFlexElement> = ({
         {...viewProps}
         className={`${viewProps?.className} ${styles.viewContainer}`}
       >
-        {renderContentView()}
+        {fileView}
       </FlexElement>
       <Text dimensionX="fill" {...nameProps} className={`${nameProps?.className} ${styles.name}`}>
-        {name}
+        {file.name}
       </Text>
     </FlexElement>
   );
