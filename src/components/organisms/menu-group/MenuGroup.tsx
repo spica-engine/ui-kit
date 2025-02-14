6fbe2617ca0bc17ba2569cd63a6a822e5041cde7
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-02-14 15:42:48 +0300

    change request

--- src/components/organisms/menu-group/MenuGroup.tsx
+++ src/components/organisms/menu-group/MenuGroup.tsx
@@ -1,14 +1,20 @@
 import React, { FC, memo } from "react";
 import FlexElement, { TypeFlexElement } from "components/atoms/flex-element/FlexElement";
-import FluidContainer from "components/atoms/fluid-container/FluidContainer";
 import Text from "components/atoms/text/Text";
 import styles from "./MenuGroup.module.scss";
 
 type TypeMenuGroup = {
-  options?: { prefix?: React.ReactNode; value: string }[][];
+  options?: Record<string, { label: string; value?: React.ReactNode }>;
+  showDivider?: boolean;
+  showTitle?: boolean;
 } & TypeFlexElement;
 
-const MenuGroup: FC<TypeMenuGroup> = ({ options = [], ...props }) => {
+const MenuGroup: FC<TypeMenuGroup> = ({
+  options = {},
+  showDivider = true,
+  showTitle = true,
+  ...props
+}) => {
   return (
     <FlexElement
       dimensionX={160}
@@ -18,29 +24,17 @@ const MenuGroup: FC<TypeMenuGroup> = ({ options = [], ...props }) => {
       className={styles.container}
       {...props}
     >
-      {options.map((section, sectionIndex) => (
+      {Object.entries(options).map(([key, option], index, array) => (
         <FlexElement
-          key={sectionIndex}
+          key={key}
           dimensionX={"fill"}
           direction="vertical"
-          gap={0}
-          className={styles.menuSection}
+          gap={10}
+          alignment="leftCenter"
+          className={`${styles.menuSection} ${!showDivider || index === array.length - 1 ? styles.noDivider : ""}`}
         >
-          {section.map((option, optionIndex) => (
-            <FluidContainer
-              gap={10}
-              key={optionIndex}
-              dimensionX={"fill"}
-              prefix={{
-                children: option.prefix,
-                dimensionX: 14,
-                dimensionY: 14,
-                className: styles.prefix,
-              }}
-              root={{ children: <Text className={styles.root}>{option.value}</Text> }}
-              className={styles.menuItem}
-            />
-          ))}
+          {showTitle && <Text textClassName={styles.label}>{option.label}</Text>}
+          {option.value}
         </FlexElement>
       ))}
     </FlexElement>
