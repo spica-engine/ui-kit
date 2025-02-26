6b67109dcc26b25711d1d0a9a031bd74fb141e36
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-02-26 15:46:46 +0300

    change request

--- src/custom-hooks/useFlexStyles.ts
+++ src/custom-hooks/useFlexStyles.ts
@@ -1,6 +1,5 @@
 import { useMemo } from "react";
 import { TypeFlexContainer, TypeFlexDimension } from "../utils/interface";
-// import "../styles/shared/global.scss";
 
 export function useFlexStyles<T extends Partial<TypeFlexContainer & TypeFlexDimension>>(
   options = {
