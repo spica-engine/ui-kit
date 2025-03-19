d6a6d353e5426b6c582699955ba111b820c822bf
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-03-19 14:21:53 +0300

    feat: add bucket rule popup

---
+++ src/components/atoms/bucket-rule-popup/BucketRulePopup.tsx
@@ -0,0 +1,72 @@
+import { Button, FluidContainer, Icon, Text } from "index.export";
+import React, { FC, memo } from "react";
+import styles from "./BucketRulePopup.module.scss";
+import TextArea from "@atoms/inputs/normal/text-area/TextArea";
+import { TypeFluidContainer } from "@atoms/fluid-container/FluidContainer";
+
+type TypeDashboardEditNameProps = {
+  name?: string;
+  onClickAddNew?: () => void;
+  onClickCancel?: () => void;
+  onClickHelp?: () => void;
+} & TypeFluidContainer;
+
+const BucketRulePopup: FC<TypeDashboardEditNameProps> = ({
+  name,
+  onClickAddNew,
+  onClickCancel,
+  onClickHelp,
+  ...props
+}) => {
+  return (
+    <FluidContainer
+      direction="vertical"
+      gap={10}
+      prefix={{
+        className: styles.prefix,
+        dimensionX: "fill",
+        alignment: "leftCenter",
+        children: (
+          <FluidContainer
+            dimensionX="fill"
+            root={{
+              alignment: "leftCenter",
+              dimensionX: "fill",
+              children: <Text className={styles.rules}>RULES</Text>,
+            }}
+            suffix={{
+              children: (
+                <Button className={styles.helpButton} variant="icon" onClick={onClickHelp}>
+                  <Icon name="help"></Icon>
+                </Button>
+              ),
+            }}
+          ></FluidContainer>
+        ),
+      }}
+      root={{
+        children: <TextArea value={name}></TextArea>,
+      }}
+      suffix={{
+        className: styles.suffix,
+        dimensionX: "fill",
+        alignment: "rightCenter",
+        children: (
+          <>
+            <Button color="transparent" variant="icon" onClick={onClickCancel}>
+              <Icon className={styles.iconCancel} name="close"></Icon>
+              <Text>Cancel</Text>
+            </Button>
+            <Button onClick={onClickAddNew}>
+              <Icon name="filter"></Icon>
+              <Text className={styles.buttonTextApply}>Add New</Text>
+            </Button>
+          </>
+        ),
+      }}
+      {...props}
+      className={`${styles.container} ${props.className}`}
+    />
+  );
+};
+export default memo(BucketRulePopup);
