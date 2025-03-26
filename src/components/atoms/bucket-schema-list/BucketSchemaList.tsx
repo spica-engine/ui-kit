be34c52cab991078cb9fd42a4665a9bf9ce31e02
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-03-26 11:13:57 +0300

    feat(BucketSchemaList): add drag & drop functionality (#166)

--- src/components/atoms/bucket-schema-list/BucketSchemaList.tsx
+++ src/components/atoms/bucket-schema-list/BucketSchemaList.tsx
@@ -1,9 +1,8 @@
-import { TypeInputType } from "@custom-hooks/useInputRepresenter";
-import { FlexElement } from "index.export";
-import React, { FC, JSX, memo } from "react";
-import styles from "./BucketSchemaList.module.scss";
+import React, { FC, JSX, memo, useState } from "react";
 import BucketSchemaItem from "@atoms/bucket-schema-item/BucketSchemaItem";
-import { TypeFlexElement } from "@atoms/flex-element/FlexElement";
+import FlexElement, { TypeFlexElement } from "@atoms/flex-element/FlexElement";
+import styles from "./BucketSchemaList.module.scss";
+import { TypeInputType } from "@custom-hooks/useInputRepresenter";
 
 type TypeSchema = {
   type: TypeInputType;
@@ -23,14 +22,42 @@ type TypeBucketSchemaList = {
 } & TypeFlexElement;
 
 const BucketSchemaList: FC<TypeBucketSchemaList> = ({ schema, itemDepth = 0, ...props }) => {
-  if (!schema) return null;
+  const [items, setItems] = useState(Object.entries(schema || {}));
+  const [expandedKeys, setExpandedKeys] = useState<Record<string, boolean>>({});
+
+  const toggleExpand = (key: string) => {
+    setExpandedKeys((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
+
+  const handleDragStart = (e: React.DragEvent, index: number) => {
+    e.dataTransfer.setData("index", index.toString());
+  };
+
+  const handleDrop = (e: React.DragEvent, targetIndex: number) => {
+    e.preventDefault();
+    const draggedIndex = Number(e.dataTransfer.getData("index"));
+
+    if (draggedIndex === targetIndex) return;
+
+    const newItems = [...items];
+    const draggedItem = newItems.splice(draggedIndex, 1)[0];
+    newItems.splice(targetIndex, 0, draggedItem);
+
+    setItems(newItems);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
 
   const renderSchemaItems = (schema: Record<string, TypeSchema>, depth: number): JSX.Element[] => {
-    return Object.entries(schema).map(([key, value]) => {
+    return Object.entries(schema).map(([key, value], index) => {
       const { type, title, properties, items, default: defaultValue, options } = value;
       const isObject = type === "object";
       const isArray = type === "array";
       const addIcon = isObject || isArray;
+      const hasChildren = isObject || isArray;
+      const expanded = expandedKeys[key] ?? false;
 
       return (
         <>
@@ -42,11 +69,19 @@ const BucketSchemaList: FC<TypeBucketSchemaList> = ({ schema, itemDepth = 0, ...
             defaultValue={defaultValue}
             options={options}
             addIcon={addIcon}
+            index={index}
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDrop={(e) => handleDrop(e, index)}
+            onDragOver={handleDragOver}
+            onClick={hasChildren ? () => toggleExpand(key) : undefined}
           />
 
-          {isObject && properties && renderSchemaItems(properties, depth + 1)}
-
-          {isArray && items && renderSchemaItems({ Item: items }, depth + 1)}
+          {expanded && (
+            <>
+              {isObject && properties && renderSchemaItems(properties, depth + 1)}
+              {isArray && items && renderSchemaItems({ Item: items }, depth + 1)}
+            </>
+          )}
         </>
       );
     });
@@ -59,7 +94,7 @@ const BucketSchemaList: FC<TypeBucketSchemaList> = ({ schema, itemDepth = 0, ...
       {...props}
       className={`${styles.container} ${props.className}`}
     >
-      {renderSchemaItems(schema, itemDepth)}
+      {renderSchemaItems(Object.fromEntries(items), itemDepth)}
     </FlexElement>
   );
 };
