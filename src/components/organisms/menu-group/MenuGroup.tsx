6c221f9379a78c1cfe5c77e93b31269bf28a405f
kenangsmv <rio.kenan@gmail.com>
2025-09-10 16:30:17 +0300

    feat: make menugroup more customizable

--- src/components/organisms/menu-group/MenuGroup.tsx
+++ src/components/organisms/menu-group/MenuGroup.tsx
@@ -3,34 +3,52 @@ import FlexElement, { TypeFlexElement } from "@atoms/flex-element/FlexElement";
 import Text from "@atoms/text/Text";
 import styles from "./MenuGroup.module.scss";
 
-export type TypeMenuGroup = {
-  options?: Record<string, { label?: string; value?: React.ReactNode; className?: string }>;
+type TypeMenuGroup = {
+  options?: Record<
+    string,
+    {
+      label?: string;
+      value?: React.ReactNode;
+      className?: string;
+    } & Partial<TypeFlexElement>
+  >;
   showDivider?: boolean;
+  showTitle?: boolean;
 } & TypeFlexElement;
 
-const MenuGroup: FC<TypeMenuGroup> = ({ options = {}, showDivider = true, ...props }) => {
+const MenuGroup: FC<TypeMenuGroup> = ({
+  options = {},
+  showDivider = true,
+  showTitle = true,
+  className,
+  ...props
+}) => {
   return (
     <FlexElement
       dimensionX={160}
       direction="vertical"
       gap={0}
       alignment="leftCenter"
-      className={styles.container}
+      className={`${styles.container} ${className || ""}`}
       {...props}
     >
-      {Object.entries(options).map(([key, option], index, array) => (
-        <FlexElement
-          key={key}
-          dimensionX={"fill"}
-          direction="vertical"
-          gap={10}
-          alignment="leftCenter"
-          className={`${styles.menuSection} ${!showDivider || index === array.length - 1 ? styles.noDivider : ""} ${option.className || ""}`}
-        >
-          {option.label && <Text className={styles.label}>{option.label}</Text>}
-          {option.value}
-        </FlexElement>
-      ))}
+      {Object.entries(options).map(([key, option], index, array) => {
+        const { label, value, className, ...flexProps } = option;
+        return (
+          <FlexElement
+            key={key}
+            dimensionX={"fill"}
+            direction="vertical"
+            gap={10}
+            alignment="leftCenter"
+            {...flexProps}
+            className={`${styles.menuSection} ${!showDivider || index === array.length - 1 ? styles.noDivider : ""} ${option.className || ""}`}
+          >
+            {label && <Text className={styles.label}>{label}</Text>}
+            {value}
+          </FlexElement>
+        );
+      })}
     </FlexElement>
   );
 };
