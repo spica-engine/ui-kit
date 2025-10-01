74e4c8d2722100684a92a0d575d66bf9f2ff1938
limonatasuyu <emredilek6@gmail.com>
2025-10-01 13:12:39 +0300

    feat: allow number inputs to be nullable for improved UX (#220)

--- src/custom-hooks/useInputRepresenter.tsx
+++ src/custom-hooks/useInputRepresenter.tsx
@@ -121,6 +121,10 @@ type TypeSelectInputProps<T extends string | number> = {
   enum?: T[];
 } & TypeInputProps<T>;
 
+type TypeNumberInput = {
+  enum?: number[];
+} & TypeInputProps<number | undefined>;
+
 type TypeMultiSelectInputProps<T extends string | number> = {
   enum?: T[];
 } & TypeInputProps<T[]>;
@@ -146,7 +150,7 @@ type TypeChipInputProps<T> = {
 
 export type TypeInputTypeMap = {
   string: (props: TypeSelectInputProps<string>) => ReactNode;
-  number: (props: TypeSelectInputProps<number>) => ReactNode;
+  number: (props: TypeNumberInput) => ReactNode;
   textarea: (props: TypeInputProps<string>) => ReactNode;
   date: (props: TypeInputProps<Date | string | null>) => ReactNode;
   boolean: (props: TypeInputProps<boolean>) => ReactNode;
