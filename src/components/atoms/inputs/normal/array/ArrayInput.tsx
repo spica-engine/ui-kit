7d2cb78dca5fb6ab31fdd6e6bb13cdea9a9093db
limonatasuyu <emredilek6@gmail.com>
2025-10-10 17:16:52 +0300

    style: align array input header padding with design

--- src/components/atoms/inputs/normal/array/ArrayInput.tsx
+++ src/components/atoms/inputs/normal/array/ArrayInput.tsx
@@ -143,6 +143,7 @@ const ArrayInput: FC<TypeArrayInput> = ({
       <div className={styles.header}>
         {title && (
           <InputHeader
+            className={styles.inputHeader}
             prefix={{ children: <Icon name="ballot" className={styles.icon} /> }}
             root={{ children: <Text variant="secondary">{title}</Text> }}
           />
