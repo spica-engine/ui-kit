8ce3b6c0374f6a6356dffbe2abc1240ffc91b391
Kenan Gasimov <kenangasimov@192.168.1.102>
2025-11-28 01:31:13 +0300

    ref: remove unnecessary codes from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import styles from "./App.module.scss";
-import { Color, ColorPicker } from "./index.export";
 
 function App() {
-  return (
-    <div className={styles.app}>
-      <Color value="#1677ff" />
-      <ColorPicker
-        defaultValue="#1677FF"
-        onChange={(color) => console.log(color.hex, color.rgb, color.hsl)}
-        format="hex"
-        placement="bottom-start"
-        disabled={false}
-      />
-    </div>
-  );
+  return <div className={styles.app}></div>;
 }
 
 export default App;
