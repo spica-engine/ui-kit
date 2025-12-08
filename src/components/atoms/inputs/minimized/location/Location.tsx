90796651d7934d504671b685ed4bb2fed5cab4af
Kenan Gasimov <kenangasimov@Kenans-MacBook-Air.local>
2025-12-08 14:15:24 +0300

    feat: add input to location minimized input

--- src/components/atoms/inputs/minimized/location/Location.tsx
+++ src/components/atoms/inputs/minimized/location/Location.tsx
@@ -1,14 +1,22 @@
-import React, { ReactNode } from "react";
-import FlexElement, { TypeFlexElement } from "@atoms/flex-element/FlexElement";
+import React, { ReactNode, useEffect, useState } from "react";
+import { TypeFlexElement } from "@atoms/flex-element/FlexElement";
 import styles from "./Location.module.scss";
 import Popover from "@atoms/popover/Popover";
-import FluidContainer from "@atoms/fluid-container/FluidContainer";
+import FluidContainer, { TypeFluidContainer } from "@atoms/fluid-container/FluidContainer";
 import locationIcon from "../../../../../assets/images/location.png";
-import Text from "@atoms/text/Text";
 import Map, { TypeMapProps } from "@atoms/map/Map";
+import { Input } from "index.export";
 
-export type TypeMinimizedLocationInput = TypeFlexElement & {
+type ChildrenProps = Omit<TypeFluidContainer, "prefix" | "root" | "suffix"> & {
+  prefix?: TypeFlexElement;
+  root?: Omit<TypeFlexElement, "children">;
+  suffix?: TypeFlexElement;
+};
+
+export type TypeMinimizedLocationInput = {
   contentProps?: TypeFlexElement;
+  childrenProps?: ChildrenProps;
+  containerProps?: TypeFlexElement;
   mapProps?: TypeMapProps;
   icon?: ReactNode;
 };
@@ -17,26 +25,70 @@ const MinimizedLocationInput = ({
   contentProps,
   mapProps,
   icon,
+  childrenProps,
+  containerProps,
   ...props
 }: TypeMinimizedLocationInput) => {
+  const [value, setValue] = useState<string>(
+    mapProps?.coordinates?.lat + " , " + mapProps?.coordinates?.lng
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+
+    const coordinatePattern = /^-?\d*\.?\d*\s*,\s*-?\d*\.?\d*$/;
+    if (inputValue === "" || coordinatePattern.test(inputValue)) {
+      setValue(inputValue);
+    }
+  };
+
+  useEffect(() => {
+    const coordinates = value.split(",");
+    if (coordinates.length === 2) {
+      const lat = Number.parseFloat(coordinates[0].trim());
+      const lng = Number.parseFloat(coordinates[1].trim());
+
+      if (!Number.isNaN(lat) && !Number.isNaN(lng)) {
+        mapProps?.onChange?.({
+          lat,
+          lng,
+        });
+      }
+    }
+  }, [value]);
+
   return (
     <Popover
-      content={<Map {...mapProps} />}
+      content={
+        <Map key={`${mapProps?.coordinates?.lat}-${mapProps?.coordinates?.lng}`} {...mapProps} />
+      }
       contentProps={{ dimensionX: 500, dimensionY: 500, ...contentProps }}
       containerProps={{ dimensionX: "fill", alignment: "leftCenter", ...props }}
     >
       <FluidContainer
         dimensionX="fill"
         dimensionY="fill"
-        prefix={{ children: icon || <img src={locationIcon} alt="location" /> }}
+        {...childrenProps}
+        prefix={{
+          children: childrenProps?.prefix?.children || icon || (
+            <img src={locationIcon} alt="location" className={styles.icon} />
+          ),
+          className: styles.iconContainer,
+        }}
         root={{
           children: mapProps?.coordinates && (
-            <Text>
-              {mapProps?.coordinates?.lat}, {mapProps?.coordinates?.lng}{" "}
-            </Text>
+            <Input
+              value={value}
+              onChange={handleChange}
+              dimensionY={"fill"}
+              className={styles.input}
+            />
           ),
+          dimensionX: "fill",
+          dimensionY: "fill",
+          className: childrenProps?.root?.className,
         }}
-        className={styles.container}
+        className={`${styles.container} ${childrenProps?.className}`}
       />
     </Popover>
   );
